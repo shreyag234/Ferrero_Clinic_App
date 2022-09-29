eab6c57300e5d35c1c1d5195567607ff1acfeda4
Shreya Ghantasala <19334780@vcconnect.co.za>
2022-09-29 21:19:51 +0200

    Done with Add_Patient page

--- Ferrero_Clinic_App/Add_Patient.aspx
+++ Ferrero_Clinic_App/Add_Patient.aspx
@@ -13,65 +13,111 @@
 <body>
     <form id="form1" runat="server">
         <div>
-            <h1 style="font-size:20px ">PATIENT INFORMATION</h1>
+            <h1 style="font-size:30px ">PATIENT INFORMATION</h1>
         </div>
         
-             <table >
+             <table style="margin-left:auto;margin-right:auto;" >
             <tr height="20dp">
             </tr>
              <tr height="10dp">
             </tr>
             <tr height="30dp">
                 <td width="20dp">&nbsp; </td>
-                <td width="20dp"><asp:Label ID="Name_LB" runat="server" Text="Name: " Font-Size="Large" ></asp:Label></td>
+                <td width="20dp"><asp:Label ID="Name_LB" runat="server" Text="Name: " Font-Size="Larger" ></asp:Label></td>
                 <td width="20dp"><asp:TextBox ID="Name_tb" runat="server"></asp:TextBox></td>
 
-                <td width="20dp"><asp:Label ID="Surname_LB" runat="server" Text="Surname: " Font-Size="Large"></asp:Label></td>
+                <td width="20dp"><asp:Label ID="Surname_LB" runat="server" Text="Surname: " Font-Size="Larger"></asp:Label></td>
                 <td width="20dp"><asp:TextBox ID="Surname_tb" runat="server" OnTextChanged="Surname_tb_TextChanged"></asp:TextBox></td>
-
-                <td width="20dp"><asp:Label ID="Maiden_LB" runat="server" Text="Maiden: " Font-Size="Large"></asp:Label></td>
+                <td width="20dp">&nbsp;</td>
+                <td width="20dp"><asp:Label ID="Maiden_LB" runat="server" Text="Maiden: " Font-Size="Larger"></asp:Label></td>
                 <td width="20dp"><asp:TextBox ID="Maiden_tb" runat="server"></asp:TextBox></td>
                 <td>&nbsp;</td>
             </tr>
+            <tr height="10dp"></tr>
             <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp; </td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td width="20dp">&nbsp;</td>
+                <td width="20dp"><asp:Label ID="Address_LB" runat="server" Text="Address:  " Font-Size="Larger"></asp:Label></td>
+                <td width="20dp"><asp:TextBox ID="Address_tb" runat="server" width="280px"></asp:TextBox> </td>
+                <td width="20dp">
+                    <asp:Label ID="City_LB" runat="server" Text="City: " Font-Size="Larger"></asp:Label></td>
+                <td width="20dp"><asp:TextBox ID="City_tb" runat="server"></asp:TextBox></td>
+                <td width="20dp">&nbsp;</td>
+                <td width="20dp"><asp:Label ID="State_LB" runat="server" Text="State: " Font-Size="Larger"></asp:Label></td>
+                <td width="20dp">
+                    <asp:TextBox ID="State_tb" runat="server" ></asp:TextBox></td>
+                <td width="20dp">&nbsp;</td>
+                <td width="20dp">
+                    <asp:Label ID="Zip_LB" runat="server" Text="Zip: " Font-Size="Larger"></asp:Label> </td>
+                <td width="20dp">
+                    <asp:TextBox ID="Zio_tb" runat="server" Width="80px"></asp:TextBox></td>
             </tr>
+            <tr height="10dp"></tr>
             <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td width="100dp"></td>
-                <td width="100dp"></td>
-                <td>&nbsp; </td>
-                <td>&nbsp;</td>
+                <td width="20dp">&nbsp;</td>
+                <td width="20dp"><asp:Label ID="DOB_LB" runat="server" Text="DOB:  " Font-Size="Larger"></asp:Label></td>
+                <td width="20dp"><asp:TextBox ID="DOB_tb" runat="server"></asp:TextBox> </td>
+                <td width="20dp">
+                    <asp:Label ID="Phone_LB" runat="server" Text="Phone: " Font-Size="Larger"></asp:Label></td>
+                <td width="20dp"><asp:TextBox ID="PhoneNum_tb" runat="server"></asp:TextBox></td>
+                <td width="20dp">&nbsp;</td>
+                
             </tr>
+        <tr height="10dp"></tr>
         <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp; </td>
+                <td width="20dp">&nbsp;</td>
+                <td width="20dp"><asp:Label ID="Email_LB" runat="server" Text="Email:  " Font-Size="Larger"></asp:Label></td>
+                <td width="20dp"><asp:TextBox ID="Eamil_tb" runat="server" width="250px"></asp:TextBox> </td>
+                <td width="20dp">
+                    <asp:Label ID="Occupation_LB" runat="server" Text="Occupation: " Font-Size="Larger"></asp:Label></td>
+                <td width="20dp"><asp:TextBox ID="Occupation_tb" runat="server"></asp:TextBox></td>
+                <td width="20dp">&nbsp;</td>
+                <td width="20dp"><asp:Label ID="Employer_LB" runat="server" Text="Employer: " Font-Size="Larger"></asp:Label></td>
+                <td width="20dp">
+                    <asp:TextBox ID="employer_tb" runat="server" ></asp:TextBox></td>
+                <td width="20dp">&nbsp;</td>
+                
             </tr>
+            <tr height="10dp"></tr>
             <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp; </td>
+                <td widht="20dp"></td>
+                <td width="20dp"><asp:Label ID="Status_LB" runat="server" Text="Marital Status: " Font-Size="Larger" Width="150px"></asp:Label></td>
+                <td widht="20dp">&nbsp;<asp:RadioButtonList ID="StatusList_rbn" runat="server" Font-Size="Larger">
+                    <asp:ListItem ID="Married_rbn" >Married</asp:ListItem>
+                    <asp:ListItem Id="Unmarried_rbn">Unmarried</asp:ListItem>
+                    <asp:ListItem ID="Divorced_rbn">Divorced</asp:ListItem>
+                    <asp:ListItem ID="Widowed_rbn">Widowed</asp:ListItem>
+                </asp:RadioButtonList></td>
+                <td width="20dp"><asp:Label ID="Spouse_LB" runat="server" Text="Spouse Fullname: " Font-Size="Larger"></asp:Label></td>
+                <td width="20dp"> <asp:TextBox ID="SpouseName_tb" runat="server" Width="200px" ></asp:TextBox></td>
+                
             </tr>
+            <tr height="10dp"></tr>
             <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                
+                <td width="20dp">&nbsp;</td>
+                <td width="20dp"><asp:Label ID="EContact_LB" runat="server" Text="Emergency Contact:  " Font-Size="Larger" Widht="350px"></asp:Label></td>
+                <td width="20dp"><asp:TextBox ID="EContact_tb" runat="server" width="200px"></asp:TextBox> </td>
+                <td width="20dp">
+                    <asp:Label ID="Relationship_LB" runat="server" Text="Relationship: " Font-Size="Larger"></asp:Label></td>
+                <td width="20dp"><asp:TextBox ID="Relation_tb" runat="server" Width="200px"></asp:TextBox></td>
+                <td width="20dp">&nbsp;</td>
+                <td width="20dp"><asp:Label ID="EPhone_LB" runat="server" Text="Emergengy Phone: " Font-Size="Larger"></asp:Label></td>
+                <td width="20dp"><asp:TextBox ID="EPhone_tb" runat="server" Width="200px"></asp:TextBox></td>
+            </tr>
+            <tr height="50dp"></tr>
+            <tr height="10dp">
+                <td widht="20dp"></td>
+                <td widht="20dp"></td>
+                <td widht="20dp"></td>
+                <td widht="20dp"></td>
+                <td widht="20dp"></td>
+                <td widht="20dp"></td>
+                <td widht="20dp"></td>
+                <td widht="20dp"></td>
+                <td widht="20dp"></td>
+                <td widht="20dp"></td>
+                <td width="20dp">
+                    <asp:Button ID="Next_btn" runat="server" Text="Next" Width="100px" Font-Bold="true" Font-Size="Larger"/></td>
             </tr>
         </table>
     </form>
--- Ferrero_Clinic_App/Add_Patient.aspx.designer.cs
+++ Ferrero_Clinic_App/Add_Patient.aspx.designer.cs
@@ -76,5 +76,302 @@ namespace Ferrero_Clinic_App
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox Maiden_tb;
+
+        /// <summary>
+        /// Address_LB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Address_LB;
+
+        /// <summary>
+        /// Address_tb control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Address_tb;
+
+        /// <summary>
+        /// City_LB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label City_LB;
+
+        /// <summary>
+        /// City_tb control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox City_tb;
+
+        /// <summary>
+        /// State_LB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label State_LB;
+
+        /// <summary>
+        /// State_tb control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox State_tb;
+
+        /// <summary>
+        /// Zip_LB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Zip_LB;
+
+        /// <summary>
+        /// Zio_tb control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Zio_tb;
+
+        /// <summary>
+        /// DOB_LB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label DOB_LB;
+
+        /// <summary>
+        /// DOB_tb control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox DOB_tb;
+
+        /// <summary>
+        /// Phone_LB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Phone_LB;
+
+        /// <summary>
+        /// PhoneNum_tb control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox PhoneNum_tb;
+
+        /// <summary>
+        /// Email_LB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Email_LB;
+
+        /// <summary>
+        /// Eamil_tb control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Eamil_tb;
+
+        /// <summary>
+        /// Occupation_LB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Occupation_LB;
+
+        /// <summary>
+        /// Occupation_tb control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Occupation_tb;
+
+        /// <summary>
+        /// Employer_LB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Employer_LB;
+
+        /// <summary>
+        /// employer_tb control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox employer_tb;
+
+        /// <summary>
+        /// Status_LB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Status_LB;
+
+        /// <summary>
+        /// StatusList_rbn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList StatusList_rbn;
+
+        /// <summary>
+        /// Married_rbn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListItem Married_rbn;
+
+        /// <summary>
+        /// Unmarried_rbn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListItem Unmarried_rbn;
+
+        /// <summary>
+        /// Divorced_rbn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListItem Divorced_rbn;
+
+        /// <summary>
+        /// Widowed_rbn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListItem Widowed_rbn;
+
+        /// <summary>
+        /// Spouse_LB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Spouse_LB;
+
+        /// <summary>
+        /// SpouseName_tb control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox SpouseName_tb;
+
+        /// <summary>
+        /// EContact_LB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label EContact_LB;
+
+        /// <summary>
+        /// EContact_tb control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox EContact_tb;
+
+        /// <summary>
+        /// Relationship_LB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Relationship_LB;
+
+        /// <summary>
+        /// Relation_tb control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Relation_tb;
+
+        /// <summary>
+        /// EPhone_LB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label EPhone_LB;
+
+        /// <summary>
+        /// EPhone_tb control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox EPhone_tb;
+
+        /// <summary>
+        /// Next_btn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Next_btn;
     }
 }
