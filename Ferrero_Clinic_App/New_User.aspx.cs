eb221edc2c003314fbcf927e363ef6de5d77bb7c
Stuart Forbes <ST10121010@vcconnect.edu.za>
2022-09-30 11:08:33 +0200

    test

--- Ferrero_Clinic_App/New_User.aspx.cs
+++ Ferrero_Clinic_App/New_User.aspx.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,11 +12,35 @@ namespace Ferrero_Clinic_App
 {
     public partial class New_User : System.Web.UI.Page
     {
+        
+        SqlConnection con = new SqlConnection("Data Source =(LocalDB)\\MSSQLLocalDB; AttachDbFilename=|DataDirectory|\\Ferrero_DBM.mdf;Integrated Security = True;");
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
 
-        
+        protected void Add_BTN_Click(object sender, EventArgs e)
+        {
+            if(Convert.ToString(User_Type_Selection_List01.SelectedItem).CompareTo("Administrator")==0)
+            {
+                byte[] p1, hash1;
+                p1 = ASCIIEncoding.ASCII.GetBytes(Password_Box01.Text);
+                hash1 = new MD5CryptoServiceProvider().ComputeHash(p1);
+                byte[] p2, hash2;
+                p2 = ASCIIEncoding.ASCII.GetBytes(Password_Box02.Text);
+                hash2 = new MD5CryptoServiceProvider().ComputeHash(p2);
+                if(hash1.Length == hash2.Length)
+                {
+                    SqlCommand cmd = new SqlCommand("insert into [dbo].[Admin](Admin_Username, Password)values(@Admin_Username,@Password)", con);
+                    cmd.Parameters.AddWithValue("@Admin_Username", Username_Box01.Text);
+                    cmd.Parameters.AddWithValue("Password", hash1);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+
+            }
+        }
     }
 }
