cea86b02cb39c2c2a5f053dcdfd6ea0a0abb475c
Stuart Forbes <ST10121010@vcconnect.edu.za>
2022-10-30 21:21:22 +0200

    yeet

---
+++ Ferrero_Clinic_App/View_Patient.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ferrero_Clinic_App
+{
+
+    public partial class View_Patient : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source =(LocalDB)\\MSSQLLocalDB; AttachDbFilename=|DataDirectory|\\Ferrero_DBM.mdf;Integrated Security = True;");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Search_BTN_01_Click(object sender, EventArgs e)
+        {
+
+            SqlDataReader rdr = null;
+            SqlCommand cmd = new SqlCommand("SELECT Patient_Name, Patient_Surname,DOB FROM Patients WHERE Patient_ID=" + Convert.ToInt32(ID_Number_TB01.Text), con);
+            con.Open();
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                PF_Name_LB01.Visible = true;
+                PF_Name_TB01.Visible = true;
+                PF_Name_TB01.Text = rdr["Patient_Name"].ToString();
+                PS_Name_LB01.Visible = true;
+                PS_Name_TB01.Visible = true;
+                PS_Name_TB01.Text = rdr["Patient_Surname"].ToString();
+                P_DOB_LB01.Visible = true;
+                P_DOB_TB01.Visible = true;
+                P_DOB_TB01.Text = rdr["DOB"].ToString().Substring(0,10);
+
+            }
+            
+ 
+        }
+    }
+}
